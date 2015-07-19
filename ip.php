f73be04631069c7cf6101622c25822566e2bc417
Mark Sturgill <msturgill@gmail.com>
2015-07-18 22:27:46 -0700

    heroku

--- ip.php
+++ ip.php
@@ -1,9 +1,10 @@
 <?php
-$ip = $_SERVER['REMOTE_ADDR'];
-if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
-  $a = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-  end($a);
-  $ip = prev($a);
-}
+//$ip = $_SERVER['REMOTE_ADDR'];
+//if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+//  $a = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+//  end($a);
+//  $ip = prev($a);
+//}
+$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 echo $ip;
 ?>
